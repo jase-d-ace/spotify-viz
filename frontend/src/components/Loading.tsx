4ab158f66a4b9dece8e35d9b4d029c88948b13c8
Jason Andrada <andrada.jasonp@gmail.com>
2025-04-25 12:52:19 -0400

    Create loading component

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import "../App.css";
+export default function Loading() {
+    return(
+        <div className="loader-container">
+            <div className="loading">
+                <div className="inner-circle"></div>
+            </div>
+        </div>
+    )
+}
