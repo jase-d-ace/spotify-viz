9e21135fe7c84c65b970a27a17de9420ea3e0222
Jason Andrada <andrada.jasonp@gmail.com>
2025-05-30 17:20:24 -0400

    create cache setter and getter

--- backend/src/cache/cache.ts
+++ backend/src/cache/cache.ts
@@ -6,10 +6,16 @@ const cache = new LRUCache<string, Analysis>({
   max: 100,
   ttl: 1000 * 60 * 5, // 5 minutes
   onInsert: (value, key, reason) => {
-    logMessage("cache", `Inserted ${key} into cache`);
+    logMessage("cache", `Inserted ${value} into cache`);
   },
   dispose: (value, key, reason) => {
-    logMessage("cache", `Deleted ${key} from cache`);
+    logMessage("cache", `Deleted ${value} from cache`);
   }
 
 });
+
+export const cacheResponse = (key: string, value: Analysis) => {
+  cache.set(JSON.stringify(key), value);
+};
+
+export const getCachedResponse = (key: string): Analysis | undefined => cache.get(JSON.stringify(key));
