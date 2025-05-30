1fca60d539e1b2a6809b8bf4f3eb703afd27d5bf
Jason Andrada <andrada.jasonp@gmail.com>
2025-05-30 17:05:40 -0400

    start building a cache

--- backend/src/cache/cache.ts
+++ backend/src/cache/cache.ts
@@ -1,5 +1,6 @@
 import { LRUCache } from "lru-cache";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Analysis } from "@types";
 import { logMessage } from "../services/logging";
 
@@ -20,11 +21,3 @@ export const cacheResponse = (key: string, value: Analysis) => {
 };
 
 export const getCachedResponse = (key: string): Analysis | undefined => cache.get(JSON.stringify(key));
-=======
-import { AnalysisResponse } from "@types";
-
-const cache = new LRUCache<string, AnalysisResponse>({
-  max: 100,
-  ttl: 1000 * 60 * 5, // 5 minutes
-});
->>>>>>> 30282a4 (start building a cache)
