36e4c13d69134defb40d0d62433c35a4373249ef
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-05-30 11:01:49 -0700

    remove ranking from Types, FE and BE

--- types/analysis.ts
+++ types/analysis.ts
@@ -1,16 +1,8 @@
 // In reference to ./backend/src/services/openai.ts schema
 
-export interface Ranking {
-  description: string;
-  letter_ranking: string;
-  number_ranking: number;
-}
-
 export interface Analysis {
   colors: string[];
   description: string;
-  // ToDo: deprecate Ranking
-  ranking?: Ranking;
 }
 
 export interface AnalysisResponse {
