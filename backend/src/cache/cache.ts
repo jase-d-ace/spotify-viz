4764dfcbab551c57f742aaa7815789c4fb575e75
Jason Andrada <andrada.jasonp@gmail.com>
2025-05-30 21:02:34 -0400

    fix logging in cache

--- backend/src/cache/cache.ts
+++ backend/src/cache/cache.ts
@@ -6,10 +6,10 @@ const cache = new LRUCache<string, Analysis>({
   max: 100,
   ttl: 1000 * 60 * 5, // 5 minutes
   onInsert: (value, key, reason) => {
-    logMessage("cache", `Inserted ${value} into cache`);
+    logMessage("cache", `Inserted ${JSON.stringify(key)} into cache`, JSON.stringify(value))
   },
   dispose: (value, key, reason) => {
-    logMessage("cache", `Deleted ${value} from cache`);
+    logMessage("cache", `Deleted ${JSON.stringify(key)} from cache`, JSON.stringify(value));
   }
 
 });
